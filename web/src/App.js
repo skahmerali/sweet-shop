6d4cd7dd7c898e865369ac67c365b4bf5984a964
Bisma456 <bismatoussef2020@gmail.com>
2021-02-19 18:23:09 +0500

    some code

---
+++ web/src/App.js
@@ -0,0 +1,22 @@
+
+import './App.css';
+// import Singup from './components/singup/singup';
+// import Login from './components/login/login';
+// import Dashbord from './components/dashbord/dashbord';
+import Nav from './components/navigation/navigation'
+
+
+
+function App() {
+  return (
+    <div className="App">
+      {/* <Singup />
+      <br />
+      <br />
+      <Login /> */}
+      <Nav />
+  </div>
+  );
+}
+
+export default App;
---
+++ web/src/components/dashbord/dashbord.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+ import Approute from "../routing/routing";
+
+function Dashbord() {
+    return (
+        <>
+        <Approute />
+            <div className="main">
+
+                <h1 className="h1-1">
+                    Dashbord
+                </h1>
+                <div className="images">
+                </div>
+            </div>
+
+        </>
+    )
+}
+export default Dashbord;
